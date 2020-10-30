e6f269b407b782556e70952bc326a52465be7f28
eagrundy <ericamalakian@gmail.com>
2020-10-29 22:18:25 -0700

    commit tree da9fe72b8b62a2a07431043f9886a81958ae3f81

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+    def initialize(name, breed="Mutt")
+        @name = name
+        @breed = breed
+end
+
+    def name=(name)
+    @name = name
+    @breed
+end
+
+    def name
+    @name
+    @breed
+end
+end
+
+fido = Dog.new("Fido", "Pug")
+fido = Dog.new("Fido")
